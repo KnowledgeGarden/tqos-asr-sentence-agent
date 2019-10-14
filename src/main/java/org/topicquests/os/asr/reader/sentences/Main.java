d4f7353789fcbd46674cc4d1d650e7480c98602f
Jack Park <jackpark@gmail.com>
2019-10-13 20:57:06 -0700

    added logs and config directories, added Main class for booting, updated pom, and start.sh

---
+++ src/main/java/org/topicquests/os/asr/reader/sentences/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.topicquests.os.asr.reader.sentences;
+
+/**
+ * @author jackpark
+ *
+ */
+public class Main {
+	private SentencesEnvironment environment;
+	
+	/**
+	 * 
+	 */
+	public Main() {
+		environment = new SentencesEnvironment();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main();
+	}
+
+}
